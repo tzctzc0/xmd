93a01334c052f3344843f7d8b221c4057a31f61c
auto-b64d <99607194+tzctzc0@users.noreply.github.com>
2024-10-09 15:50:01 +0900

    범용화

--- panel.js
+++ panel.js
@@ -9,7 +9,7 @@ chrome.devtools.network.onRequestFinished.addListener(req => {
 				.data
 				.threaded_conversation_with_injections_v2
 				.instructions
-			itemsEl.append(...createVideoItemEls(insts))
+			itemsEl.append(...createTweetItemEls(insts))
 			itemsEl.scrollTop = itemsEl.scrollHeight
 		})
 	} else if (req.request.url.includes('/UserTweets')) {
@@ -23,32 +23,32 @@ chrome.devtools.network.onRequestFinished.addListener(req => {
 				.timeline_v2
 				.timeline
 				.instructions
-			itemsEl.append(...createVideoItemEls(insts))
+			itemsEl.append(...createTweetItemEls(insts))
 			itemsEl.scrollTop = itemsEl.scrollHeight
 		})
 	}
 })
 
-const createVideoItemEls = instructions => {
+const createTweetItemEls = instructions => {
 	const results = []
 	const tweetEntries = instructions
 		.find(inst => inst.type === 'TimelineAddEntries')
 		.entries
 	for (const tweetEntry of tweetEntries) {
 		if (tweetEntry.entryId.startsWith('tweet-')) {
-			const videoItem = createVideoItemEl(
+			const mediaItem = createTweetItemEl(
 				tweetEntry
 					.content
 					.itemContent
 					.tweet_results
 					.result
 			)
-			if (videoItem) results.push(videoItem)
+			if (mediaItem) results.push(mediaItem)
 		} else if (tweetEntry.entryId.startsWith('profile-conversation-')) {
 			results.push(
 				...tweetEntry.content.items
 					.map(entry =>
-						createVideoItemEl(
+						createTweetItemEl(
 							entry
 								.item
 								.itemContent
@@ -63,14 +63,22 @@ const createVideoItemEls = instructions => {
 	return results
 }
 
-const createVideoItemEl = tweet => {
+const createTweetItemEl = tweet => {
 	// 특정 기능이 제한된 트윗은 구조가 또 다름
 	if (tweet.__typename === 'TweetWithVisibilityResults') tweet = tweet.tweet
-
+	
+	const isTombstone = tweet.__typename === 'TweetTombstone'
+	if (isTombstone) return null
+	
 	const isRetweet = 'retweeted_status_result' in tweet.legacy
 	if (isRetweet) return null
 	
-	const tweetContent = tweet.legacy.full_text
+	// display_text_range는 코드 포인트 기준이므로
+	// 멀티바이트 문자가 포함된 경우 문자열에 바로 .slice()하면 끝부분이 잘릴 수 있음
+	const tweetContent =
+		[...tweet.legacy.full_text]
+			.slice(...tweet.legacy.display_text_range)
+			.join('')
 	const medias = tweet.legacy.entities.media ?? []
 	
 	const tweetEl = document.createElement('article')
@@ -117,38 +125,59 @@ const createVideoItemEl = tweet => {
 		tweetContentEl,
 	)
 	
-	const videoListEl = document.createElement('ul')
-	videoListEl.classList.add('tweet-video-list')
-	tweetEl.append(contentEl, videoListEl)
+	const mediaListEl = document.createElement('ul')
+	mediaListEl.classList.add('tweet-media-list')
+	tweetEl.append(contentEl, mediaListEl)
 	
 	let i = 0
 	for (const media of medias) {
-		if (media.type !== 'video') continue
+		if (!['video', 'photo', 'animated_gif'].includes(media.type)) continue
 		
-		const videoIdx = i
+		const mediaIdx = i
 		if (document.querySelector(`[data-url="${media.expanded_url}"]`)) continue
 		
-		const videoInfo = media.video_info
-		const duration = videoInfo.duration_millis
-		const bestVariant = videoInfo.variants
-			.slice(1) // remove element containing mpeg url that we are not interested
-			.sort((a, b) =>  b.bitrate - a.bitrate)
-			[0]
-		
-		const videoItemEl = document.createElement('li')
-		videoItemEl.classList.add('video-item')
-		videoItemEl.dataset.url = media.expanded_url
+		let downloadUrl
+		let headContent
+		let fileExt
+		const originalSize = `${media.original_info.width}x${media.original_info.height}`
+		if (media.type === 'photo') {
+			let highestSizeName
+			for (const sizeName in media.sizes) {
+				const size = media.sizes[sizeName]
+				const highestSize = media.sizes[highestSizeName]
+				if (!highestSize || size.w * size.h > highestSize.w * highestSize.h) highestSizeName = sizeName
+			}
+			downloadUrl = `${media.media_url_https}?name=${highestSizeName}`
+			headContent = `image / ${originalSize}`
+			fileExt = 'jpg'
+		} else {
+			const videoInfo = media.video_info
+			const bestVariant = videoInfo.variants
+				.filter(v => v.content_type !== 'application/x-mpegURL')
+				.sort((a, b) => b.bitrate - a.bitrate)
+				[0]
+			
+			downloadUrl = bestVariant.url
+			headContent =
+				media.type === 'video'
+					? `video / ${originalSize} / ${videoInfo.duration_millis}ms`
+					: `GIF / ${originalSize}`
+			fileExt = 'mp4'
+		}
+		const mediaItemEl = document.createElement('li')
+		mediaItemEl.classList.add('media-item')
+		mediaItemEl.dataset.url = media.expanded_url
 		
 		const downloadBtnEl = document.createElement('button')
 		downloadBtnEl.innerText = 'Download'
 		downloadBtnEl.addEventListener('click', () => {
 			tweetEl.dataset.downloadStatus = 'pending'
-			fetch(bestVariant.url)
+			fetch(downloadUrl)
 				.then(res => res.blob())
 				.then(blob => {
 					const linkEl = document.createElement('a')
 					linkEl.href = URL.createObjectURL(blob)
-					linkEl.download = `${tweet.rest_id}_${videoIdx}.mp4`
+					linkEl.download = `${tweet.rest_id}_${mediaIdx}.${fileExt}`
 					document.body.append(linkEl)
 					linkEl.click()
 					linkEl.remove()
@@ -162,12 +191,11 @@ const createVideoItemEl = tweet => {
 		
 		const headEl = document.createElement('span')
 		headEl.classList.add('head')
-		headEl.innerText = `Duration: ${duration}ms`
-		
-		videoItemEl.append(headEl, downloadBtnEl)
-		videoListEl.append(videoItemEl)
+		headEl.innerText = headContent
+		mediaItemEl.append(headEl, downloadBtnEl)
+		mediaListEl.append(mediaItemEl)
 		i++
 	}
 	
-	return videoListEl.children.length !== 0 ? tweetEl : null
+	return mediaListEl.children.length !== 0 ? tweetEl : null
 }
